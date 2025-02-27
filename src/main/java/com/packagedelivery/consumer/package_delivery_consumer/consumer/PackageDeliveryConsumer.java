5e319483d8d3093d48f52568ac1cd959850540c9
danielaser <daniela.serrano.dev@gmail.com>
2025-02-26 19:27:36 -0500

    feat(develop): add services and consumer

--- src/main/java/com/packagedelivery/consumer/package_delivery_consumer/consumer/PackageDeliveryConsumer.java
+++ src/main/java/com/packagedelivery/consumer/package_delivery_consumer/consumer/PackageDeliveryConsumer.java
@@ -1,4 +1,22 @@
 package com.packagedelivery.consumer.package_delivery_consumer.consumer;
 
+import com.packagedelivery.consumer.package_delivery_consumer.dtos.Tracking;
+import com.packagedelivery.consumer.package_delivery_consumer.services.TrackingService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PackageDeliveryConsumer {
+
+    private final TrackingService trackingService;
+
+    public PackageDeliveryConsumer(TrackingService trackingService) {
+        this.trackingService = trackingService;
+    }
+
+    @RabbitListener(queues = "package_delivery_queue")
+    public void receiveMessage(Tracking tracking) {
+        System.out.println("Received message " + tracking);
+        trackingService.saveTracking(tracking);
+    }
 }
--- src/main/java/com/packagedelivery/consumer/package_delivery_consumer/dtos/Tracking.java
+++ src/main/java/com/packagedelivery/consumer/package_delivery_consumer/dtos/Tracking.java
@@ -1,4 +1,29 @@
 package com.packagedelivery.consumer.package_delivery_consumer.dtos;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tracking")
 public class Tracking {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Status status;
+    private String agency;
+    private LocalDateTime arrivalTime;
+    private String address;
+
+    @OneToMany(mappedBy = "tracking", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Package> packages;
+
 }
