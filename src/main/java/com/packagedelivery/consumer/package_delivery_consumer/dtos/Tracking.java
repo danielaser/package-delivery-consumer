4e15da56c8667a91d125e201961ebe31204adedd
danielaser <daniela.serrano.dev@gmail.com>
2025-02-28 08:25:39 -0500

    feat(develop): add atributes

--- src/main/java/com/packagedelivery/consumer/package_delivery_consumer/dtos/Tracking.java
+++ src/main/java/com/packagedelivery/consumer/package_delivery_consumer/dtos/Tracking.java
@@ -2,14 +2,16 @@ package com.packagedelivery.consumer.package_delivery_consumer.dtos;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "tracking")
@@ -18,15 +20,14 @@ public class Tracking {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private TrackingStatus status;
-
     private String agency;
     private LocalDateTime arrivalTime;
+    @Enumerated(EnumType.STRING)
+    private TrackingStatus status;
     private String address;
 
     @OneToMany(mappedBy = "tracking", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Package> packages;
 
-    private Double price;
+    private Double total;
 }
